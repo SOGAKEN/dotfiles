fcd6fb933f4a8f3c8ffa8d2f5f63ea8e8807de52
SOGAKEN <sogaken0919@gmail.com>
2023-02-21 11:59:46 +0900

    change neovim => Lazy nbim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,16 +1,2 @@
-require('sogaken.base')
-require('sogaken.highlights')
-require('sogaken.maps')
-require('sogaken.plugins')
--- Lua:
-vim.cmd[[colorscheme dracula]]
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-local is_mac = has "macunix"
-
-if is_mac then
-  require('sogaken.macos')
-end
-
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
